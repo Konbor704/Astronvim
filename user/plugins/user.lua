dfb540c3992c2ec1ce031ef4b589d5ce33726764
Konbor <konbor12@gmail.com>
2023-09-08 07:54:18 +0200

    start

--- user/plugins/user.lua
+++ user/plugins/user.lua
@@ -63,24 +63,10 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
   },
+  { "nvim-treesitter/nvim-treesitter" },
   {
-    "nvim-neorg/neorg",
-    build = ":Neorg sync-parsers",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("neorg").setup {
-        load = {
-          ["core.defaults"] = {}, -- Loads default behaviour
-          ["core.concealer"] = {}, -- Adds pretty icons to your documents
-          ["core.dirman"] = { -- Manages Neorg workspaces
-            config = {
-              workspaces = {
-                notes = "~/Documents/Notes/",
-              },
-            },
-          },
-        },
-      }
-    end,
+    "nvim-orgmode/orgmode",
+    ft = { "org" },
+    config = function() require("orgmode").setup {} end,
   },
 }
